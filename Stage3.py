bc293613a1ccd1f88988b50549f2c3582ccf999f
Jake McLaughlin <jmmclaughlin@monmouthcollege.edu>
2021-04-29 14:15:32 -0500

    Edited stage3.py to work with dataframe.

--- Stage3.py
+++ Stage3.py
@@ -3,12 +3,11 @@ import plotly as pl
 import ipstack as i
 from ipstack import GeoLookup
 
-df = pd.read_csv("LocationInfo.csv")
 geo_lookup = GeoLookup("004fe3a3435401b81ac4ea493a0834cf")
 
 def stage3(df):
     for a in df.index:
-        current_ip = df.loc[a, 'IP']
+        current_ip = df.loc[a, 'ip']
         location = geo_lookup.get_location(current_ip)
         continent = location["continent_name"]
         country = location["country_name"]
