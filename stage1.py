5d153bae7948d9d4691ad44ba7526c80e5d9378d
cwillhardt <cwillhardt85@yahoo.com>
2021-05-02 14:40:28 -0500

    RE in stage 1

--- stage1.py
+++ stage1.py
@@ -2,6 +2,7 @@
 
 import os
 import csv
+import re
 
 # gets desired date ip pairs from logs and write to csv
 def stage1():
@@ -11,41 +12,12 @@ def stage1():
     # loop through all fail2ban logs in the log directory
     for filename in os.listdir(path):
         print(filename)
-        file = open(path+'/'+filename,'r')
-        lines = file.readlines()
-        for line in lines:
-            if line_check(line):
-                csv_writer.writerow([get_date(line),get_ip(line)])
-        file.close()
+        file = open(path+'/'+filename,'r').read()
+        for line in re.findall(".*fail2ban.actions.*NOTICE.*",file):
+            date = re.split(",",line)[0]
+            ip = re.split('\s',line).pop()
+            csv_writer.writerow([date,ip])
     output.close()
-                
-# checks if a line in the fail2ban log is of interest            
-def line_check(line):
-    if 'fail2ban.actions' in line and 'NOTICE' in line:
-        return True
-
-# extracts the date from line in fail2ban log
-def get_date(line):
-    date = ''
-    # loop through string until we see a comma
-    for char in line:
-        if char != ',':
-            date += char
-        else:
-            break
-    return date
-
-# extracts the ip from line in fail2ban log
-def get_ip(line):
-    ip = ''
-    # loop through string in reverse until we see a space
-    for i in range(len(line)-1,-1,-1):
-        char = line[i]
-        if char != ' ':
-           ip = char + ip
-        else:
-            break
-    return ip
 
 
 if __name__ == "__main__":
